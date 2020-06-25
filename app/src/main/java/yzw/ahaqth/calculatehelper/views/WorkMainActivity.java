c171d226be747366cc138f289a29106da7ba7a1a
IronEgg1980 <yinzongwang@163.com>
2020-06-25 21:57:02 +0800

    20200625-3

--- app/src/main/java/yzw/ahaqth/calculatehelper/views/WorkMainActivity.java
+++ app/src/main/java/yzw/ahaqth/calculatehelper/views/WorkMainActivity.java
@@ -1,263 +1,89 @@
 package yzw.ahaqth.calculatehelper.views;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.mcxtzhang.swipemenulib.SwipeMenuLayout;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.List;
 
 import yzw.ahaqth.calculatehelper.R;
-import yzw.ahaqth.calculatehelper.moduls.RecordDetailsGroupByItem;
-import yzw.ahaqth.calculatehelper.moduls.Remain;
-import yzw.ahaqth.calculatehelper.tools.DateUtils;
-import yzw.ahaqth.calculatehelper.tools.DbManager;
-import yzw.ahaqth.calculatehelper.views.adapters.MyAdapter;
 import yzw.ahaqth.calculatehelper.views.dialogs.ToastFactory;
+import yzw.ahaqth.calculatehelper.views.interfaces.InputDataFragment;
 
 public class WorkMainActivity extends AppCompatActivity {
     private String TAG = "殷宗旺";
-    private List<RecordDetailsGroupByItem> dataList;
-    private MyAdapter<RecordDetailsGroupByItem> adapter;
-    private RecyclerView recyclerView;
+    private final String BUNDLE_FLAG = "recordtime",CURRENT_FRAGMENT_FLAG = "currentfragment";
     private LocalDateTime recordTime;
-    private TextView recordTimeTextView, titleTextView;
-    private View slideMenu,menuToggle;
-    private AnimatorSet showAnimatorSet,hideAnimatorSet;
-    private boolean hasData = false;
+    private int currentFragmentFlag ;
 
-    private void initial() {
-        dataList = new ArrayList<>();
-        adapter = new MyAdapter<RecordDetailsGroupByItem>(dataList) {
-            @Override
-            public int getLayoutId(int position) {
-                return R.layout.recordgroupbyitem_item_layout;
-            }
-
-            @Override
-            public void bindData(final MyViewHolder myViewHolder, final RecordDetailsGroupByItem data) {
-                myViewHolder.setText(R.id.itemname_textview, data.getItemName());
-                myViewHolder.setText(R.id.amount_textview, "总金额：" + data.getTotalAmount());
-                myViewHolder.setText(R.id.note_textview, data.getMonthNote());
-                final SwipeMenuLayout menuLayout = myViewHolder.getView(R.id.swipeMenuLayout);
-                myViewHolder.getView(R.id.swipe_menu_del).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        menuLayout.smoothClose();
-                        DbManager.deleInputRecord(data);
-                        readData();
-                    }
-                });
-            }
-        };
-    }
-
-    private void readData() {
-        String s = "当前记录时间：" + recordTime.format(DateUtils.getYyyyMdHHmmss_Formatter());
-        recordTimeTextView.setText(s);
-        dataList.clear();
-        dataList.addAll(DbManager.getRecordGroupByItem(recordTime));
-        adapter.notifyDataSetChanged();
-    }
-
-    private void showMenu(){
-        if(showAnimatorSet == null){
-            ObjectAnimator showAnimator = ObjectAnimator.ofFloat(slideMenu,"translationX",-slideMenu.getWidth(),0);
-            showAnimator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationStart(Animator animation) {
-                    slideMenu.setVisibility(View.VISIBLE);
-                    super.onAnimationStart(animation);
-                }
-            });
-
-            ObjectAnimator toggle2Right = ObjectAnimator.ofFloat(menuToggle,"translationX",-slideMenu.getWidth(),0);
-            showAnimatorSet = new AnimatorSet();
-            showAnimatorSet.playTogether(showAnimator,toggle2Right);
-            showAnimatorSet.setDuration(200);
-        }
-        if(slideMenu.getVisibility() != View.VISIBLE)
-            showAnimatorSet.start();
-    }
-
-    private void hideMenu(){
-        if(hideAnimatorSet == null){
-            ObjectAnimator toggle2Left = ObjectAnimator.ofFloat(menuToggle,"translationX",0,-slideMenu.getWidth());
-
-            ObjectAnimator hideAnimator = ObjectAnimator.ofFloat(slideMenu,"translationX",0,-slideMenu.getWidth());
-            hideAnimator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
-                    slideMenu.setVisibility(View.INVISIBLE);
-                }
-            });
-
-            hideAnimatorSet = new AnimatorSet();
-            hideAnimatorSet.setDuration(200);
-            hideAnimatorSet.playTogether(hideAnimator,toggle2Left);
-        }
-        if(slideMenu.getVisibility()==View.VISIBLE)
-            hideAnimatorSet.start();
-    }
-
-    private void initialView() {
-        slideMenu = findViewById(R.id.slideMenu);
-        menuToggle = findViewById(R.id.menuToggle);
-        menuToggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(slideMenu.getVisibility() == View.VISIBLE)
-                    hideMenu();
-                else
-                    showMenu();
-            }
-        });
-        recordTimeTextView = findViewById(R.id.recordtime_textview);
-        titleTextView = findViewById(R.id.titleTextView);
-        titleTextView.setText("工作区 - 记录列表");
-        recyclerView = findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if(newState != RecyclerView.SCROLL_STATE_IDLE && slideMenu.getVisibility()==View.VISIBLE)
-                    hideMenu();
-            }
-        });
-
-        findViewById(R.id.navagationIco).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-
-        findViewById(R.id.inputButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                jumpToInput();
-            }
-        });
-        findViewById(R.id.assignButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WorkMainActivity.this, AssignActivity.class);
-                intent.putExtra("recordtime", recordTime.toEpochSecond(ZoneOffset.ofHours(8)));
-                startActivity(intent);
-            }
-        });
-        findViewById(R.id.personManageButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(WorkMainActivity.this, PersonManageActivity.class));
-            }
-        });
-        findViewById(R.id.itemManageButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(WorkMainActivity.this, ItemManageActivity.class));
-            }
-        });
-        findViewById(R.id.remainAssignButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showRemain((TextView) v);
-            }
-        });
-    }
-
-    private void jumpToInput() {
-        Intent intent = new Intent(WorkMainActivity.this, InputActivity.class);
-        intent.putExtra("recordtime", recordTime.toEpochSecond(ZoneOffset.ofHours(8)));
-        startActivity(intent);
-    }
-
-    private void showRemain(TextView textView) {
-        Remain remain = DbManager.findFirst(Remain.class);
-        double remainValue = remain != null ? remain.getAmount() : 0;
-        ToastFactory.showCenterToast(this, String.valueOf(remainValue));
+    public LocalDateTime getRecordTime(){
+        return this.recordTime;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong(BUNDLE_FLAG,recordTime.toEpochSecond(ZoneOffset.ofHours(8)));
+        outState.putInt(CURRENT_FRAGMENT_FLAG,currentFragmentFlag);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        long time = savedInstanceState.getLong(BUNDLE_FLAG);
+        this.recordTime = LocalDateTime.ofEpochSecond(time,0,ZoneOffset.ofHours(8));
+        this.currentFragmentFlag = savedInstanceState.getInt(CURRENT_FRAGMENT_FLAG);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_main);
         recordTime = LocalDateTime.now();
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            hasData = true;
-            long l = bundle.getLong("recordtime");
-            recordTime = LocalDateTime.ofEpochSecond(l, 0, ZoneOffset.ofHours(8));
+        currentFragmentFlag = 1 ;
+        if(savedInstanceState != null){
+            long time = savedInstanceState.getLong(BUNDLE_FLAG);
+            this.recordTime = LocalDateTime.ofEpochSecond(time,0,ZoneOffset.ofHours(8));
+            this.currentFragmentFlag = savedInstanceState.getInt(CURRENT_FRAGMENT_FLAG);
         }
-//        new Test(this).test();
-        initial();
         initialView();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if(hasData) {
-            readData();
-            slideMenu.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    hideMenu();
-                }
-            },500);
+        if(currentFragmentFlag == 1){
+            changeToInputData();
         }else{
-            hasData = true;
-            jumpToInput();
+            changeToAssign();
         }
     }
 
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        recordTime = LocalDateTime.ofEpochSecond(savedInstanceState.getLong("recordtime"), 0, ZoneOffset.ofHours(8));
+    public void changeToInputData(){
+        currentFragmentFlag = 1;
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer,new InputDataFragment())
+                .commit();
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putLong("recordtime", recordTime.toEpochSecond(ZoneOffset.ofHours(8)));
-        super.onSaveInstanceState(outState);
+    public void changeToAssign(){
+        currentFragmentFlag = 2;
+        ToastFactory.showCenterToast(this,"change to assign");
     }
 
-//    private void showDialog() {
-//        DialogFactory confirmDialog = DialogFactory.getConfirmDialog("存在未分配的记录，是否清除数据重新输入？");
-//        confirmDialog.setDialogCallback(new DialogCallback() {
-//            @Override
-//            public void onDismiss(boolean confirmFlag, Object... values) {
-//                hasUnsignedData = false;
-//                if (confirmFlag) {
-////                    DbManager.deleAll(RecordDetails.class,"datamode = ?", String.valueOf(DataMode.UNASSIGNED.ordinal()));
-//                    DbManager.clearOldInputData();
-//                    recordTime = LocalDateTime.now();
-//                    jumpToInput();
-//                } else {
-//                    readData();
-//                }
-//            }
-//        });
-//        confirmDialog.show(getSupportFragmentManager(),"dialog");
-//    }
+    private void initialView(){
+        TextView titleView = findViewById(R.id.titleTextView);
+        titleView.setText("工作区域");
+        findViewById(R.id.navagationIco).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 }
--- app/src/main/java/yzw/ahaqth/calculatehelper/views/interfaces/InputDataFragment.java
+++ app/src/main/java/yzw/ahaqth/calculatehelper/views/interfaces/InputDataFragment.java
@@ -1,7 +1,122 @@
 package yzw.ahaqth.calculatehelper.views.interfaces;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import yzw.ahaqth.calculatehelper.R;
+import yzw.ahaqth.calculatehelper.moduls.RecordDetails;
+import yzw.ahaqth.calculatehelper.moduls.RecordDetailsGroupByItem;
+import yzw.ahaqth.calculatehelper.tools.DateUtils;
+import yzw.ahaqth.calculatehelper.views.MyDivideItemDecoration;
+import yzw.ahaqth.calculatehelper.views.WorkMainActivity;
+import yzw.ahaqth.calculatehelper.views.adapters.MyAdapter;
 
 public class InputDataFragment extends Fragment {
+    private WorkMainActivity activity;
+    private MaterialEditText itemNameET,amountET;
+    private RecyclerView resultRecyclerView,monthListRecyclerView;
+    private List<RecordDetails> recordDetailsList;
+    private List<RecordDetailsGroupByItem> resultList;
+    private MyAdapter<RecordDetails> monthListAdapter;
+    private MyAdapter<RecordDetailsGroupByItem> resultAdapter;
 
+    private void initial(){
+        activity = (WorkMainActivity) getActivity();
+        resultList = new ArrayList<>();
+        initialRecordDetailsList();
+
+        monthListAdapter = new MyAdapter<RecordDetails>(recordDetailsList) {
+            @Override
+            public void bindData(MyViewHolder myViewHolder, RecordDetails data) {
+                if(data.getDataMode() == DataMode.EMPTY){
+
+                }else{
+                    myViewHolder.setText(R.id.monthTextView,data.getMonth().format(DateUtils.getYyM_Formatter()));
+                    myViewHolder.setText(R.id.amountTextView,String.valueOf(data.getAmount()));
+                }
+            }
+
+            @Override
+            public int getLayoutId(int position) {
+                if(recordDetailsList.get(position).getDataMode()==DataMode.EMPTY)
+                    return R.layout.select_month_item_addbutton;
+                return R.layout.select_month_item;
+            }
+        };
+
+        resultAdapter = new MyAdapter<RecordDetailsGroupByItem>(resultList) {
+            @Override
+            public void bindData(MyViewHolder myViewHolder, RecordDetailsGroupByItem data) {
+
+            }
+
+            @Override
+            public int getLayoutId(int position) {
+                return R.layout.recordgroupbyitem_item_layout;
+            }
+        };
+    }
+
+    private void initialRecordDetailsList(){
+        recordDetailsList = new ArrayList<>();
+        LocalDate month = LocalDate.now();
+        for(int i = 0;i<6;i++){
+            RecordDetails recordDetails = new RecordDetails();
+            recordDetails.setRecordTime(activity.getRecordTime());
+            recordDetails.setMonth(month.minusMonths(i));
+            recordDetails.setAmount(0);
+            recordDetails.setDataMode(DataMode.UNASSIGNED);
+            recordDetails.setItemName("");
+            recordDetailsList.add(recordDetails);
+        }
+        RecordDetails recordDetails2 = new RecordDetails();
+        recordDetails2.setDataMode(DataMode.EMPTY);
+        recordDetailsList.add(recordDetails2);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initial();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_input_data_layout,container,false);
+        view.findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.changeToAssign();// 进入下一步
+            }
+        });
+        itemNameET = view.findViewById(R.id.itemname_textview);
+        amountET = view.findViewById(R.id.amount_textview);
+        monthListRecyclerView = view.findViewById(R.id.month_list_recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        monthListRecyclerView.setLayoutManager(linearLayoutManager);
+        monthListRecyclerView.setAdapter(monthListAdapter);
+
+        resultRecyclerView = view.findViewById(R.id.result_recyclerview);
+        resultRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        resultRecyclerView.setAdapter(resultAdapter);
+        resultRecyclerView.addItemDecoration(new MyDivideItemDecoration());
+        return view;
+    }
 }
